02ca2ef9bcc56606b321cb94f631eacc39005069
Muhammad Shaheer <ks9612684@gmail.com>
2025-02-27 21:16:21 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-let imgbox = document.getElementById("imgbox");
-let qrImage = document.getElementById("qr-img");
-let input = document.getElementById("qr-text")
-let button = document.getElementById("btn");
+const imgbox = document.getElementById("imgbox");
+const qrImage = document.getElementById("qr-img");
+const input = document.getElementById("qr-text");
+const button = document.getElementById("btn");
 
-
-function generateQr(event){
-    qrImage.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+input.value;
-
-
-
+function generateQr() {
+    const inputValue = input.value.trim();
+    if (inputValue) {
+        qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(inputValue)}`;
+        imgbox.style.display = 'block';
+    } else {
+        alert("Please enter a valid link or text!");
+    }
 }
 
-
-button.addEventListener("click",(event)=>{
-    generateQr();
-})
+button.addEventListener("click", generateQr);
